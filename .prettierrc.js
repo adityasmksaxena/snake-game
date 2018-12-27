8cd7e34a7b88575e9ba870c7af321955f6e7a042
adi <aditya.saxena@appstreet.io>
2018-12-27 14:52:56 +0530

    init: project setup

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+const prettierRules = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'es5',
+};
+
+module.exports = prettierRules;
---
+++ node-server/server.js
@@ -0,0 +1,25 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const { isProduction } = require('./util/util.js');
+const app = express();
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+if (isProduction) {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    path.resolve(__dirname, 'client', 'build', 'index.html');
+  });
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+module.export = app;
